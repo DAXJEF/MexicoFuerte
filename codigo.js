ac2e7d0cccacae95ec7a187f5bd04d4dd105b674
DAXJEF <81991297+DAXJEF@users.noreply.github.com>
2021-04-05 14:57:42 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,15 @@
+alert ("Hola, bienvenido al registro para becas")
+document.write ("Esperamos que esta pagina sea de tu agrado.")
+var x=1
+var nombre=prompt ("Igrese su nombre en este campo para continuar")
+document.write ("mensaje de variable; "+x)
+alert ("Mensaje de variable "+x+ "El nombre es "+nombre)
+
+
+
+
+
+
+    
+    
+	
