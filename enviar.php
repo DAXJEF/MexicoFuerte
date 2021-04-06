da13bf9c8c91d13d3a6fab96a5c8d9346c60b581
DAXJEF <81991297+DAXJEF@users.noreply.github.com>
2021-04-05 19:01:27 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,25 +1,21 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['mail'];
-$phone = $_POST['phone'];
-$message = $_POST['message'];
 
-$header = 'From: ' . $mail . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+//llamando a los campos
+$nombre = $_POST['name'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
 
-$message = "Este mensaje fue enviado por: " . $name . " \r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
-$message .= "Teléfono de contacto: " . $phone . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+//Datos para el correo
+$destinatario = "daxterjef2@gmail.com";
+$asunto = "contacto desde nuesta web";
 
-$para = "daxterjef2@gmail.com";
-$asunto = "Formulario de contacto";
+$carta ="De: $nombre \n";
+$carta .="Correo $correo \n";
+$carta .="Telefono $telefono \n";
+$carta .="Mensaje $mensaje";
 
-mail($para, $asunto, utf8_decode($message), $header);
+//Enviando Mensaje
+mail($destinatario, $asunto, $carta);
 
-header("Location:contacto.html");
 ?>
--->
